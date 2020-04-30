d2e6861046c8df7935f4220c4a78d098cce02e9a
Kegon <kegon@balancetheory.com>
2020-04-30 18:54:32 -0400

    REFACTORED: constructors to allow objects to be passed as first parameter;

--- main.mjs
+++ main.mjs
@@ -8,27 +8,24 @@ import MockUp from './lib/activities.mock.mjs';
 ((doc) => {
 
   // Get Events Graph and 
-  const graph = new EventsGraph( 'olycloud', doc.getElementById('3d-graph') )
+  const graph = new EventsGraph( 'olycloud', doc.getElementById('3d-graph') );
 
   //load graph data
   graph.loadGraphDataFromUrl('./data/olycloud.graph.data.json', (data) => {
-    console.log("loadDataFromUrl CB ", data);
 
     // Start Mock Activity
     MockUp.startMockEGEvents(graph);
     //MockUp.startMockEGUsers();
   });
 
-  doc.getElementById('add-user-btn').addEventListener('click', (e) => {
-    //console.log('---> Click: ', e);
-    // MockUp.createRandomEvent();
-    MockUp.createRandomUser(graph);
-  });
+  doc.getElementById('add-user-btn')
+    .addEventListener('click', (e) => {
+      MockUp.createRandomUser(graph);
+    });
 
-  doc.getElementById('remove-user-btn').addEventListener('click', (e) => {
-    //console.log('---> Click: ', e);
-    // MockUp.createRandomEvent();
-    MockUp.removeRandomUser(graph);
-  });
+  doc.getElementById('remove-user-btn')
+    .addEventListener('click', (e) => {
+      MockUp.removeRandomUser(graph);
+    });
 
 })(document);
